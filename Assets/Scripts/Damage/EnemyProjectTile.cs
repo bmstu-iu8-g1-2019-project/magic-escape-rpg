4ff70592a77de311b47cc213a112ac8cb4e73972
exeynod <exeynod@gmail.com>
2019-10-27 17:12:05 +0300

    Add new weapon - Arrow

---
+++ Assets/Scripts/Damage/EnemyProjectTile.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyProjectTile : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Time.timeScale == 0f)
+        {
+            return;
+        }
+        if (!collision.CompareTag("Enemy") && !collision.CompareTag("Spawner") 
+            && !collision.CompareTag("Damage") && !collision.CompareTag("PlayerDamage")
+            && !collision.CompareTag("Scaner"))
+        {
+            Destroy(this.gameObject, 0.1f);
+        }
+    }
+}
