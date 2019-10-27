16cecce991c05a1f931682ee17f256179bda54a6
exeynod <exeynod@gmail.com>
2019-10-27 17:17:37 +0300

    Add PLayer aim-helper

---
+++ Assets/Scripts/Player/Scaner.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scaner : MonoBehaviour
+{
+    private GameObject Player;
+    private CircleCollider2D Collider;
+    [SerializeField] private float deltaR;
+    [SerializeField] private float MaxR;
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Collider = gameObject.GetComponent<CircleCollider2D>();
+    }
+
+    void Update()
+    {
+        if (Collider.radius < MaxR)
+        {
+            Collider.radius += deltaR * Time.deltaTime;
+        }         
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy") && !collision.GetComponent<Enemy>().IsDead())
+        {
+            Player.GetComponent<PlayerManager>().Target = collision.gameObject; 
+            Collider.radius = 0;
+        }
+    }
+}
