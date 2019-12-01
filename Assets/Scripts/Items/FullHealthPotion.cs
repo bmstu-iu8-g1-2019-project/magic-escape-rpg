7911ab304dce4a6c1bd9e63952f4cacf2b4809c3
exeynod <exeynod@gmail.com>
2019-12-01 23:48:13 +0300

    created ItemCreation.md

---
+++ Assets/Scripts/Items/FullHealthPotion.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FullHealthPotion : MonoBehaviour
+{
+    public void OnUse()
+    {
+        PlayerManager player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerManager>();
+        player.CurrentHealth.RuntimeValue = player.CurrentHealth.InitialValue;
+        player.PlayerHealthSignal.Raise();
+    }
+}
