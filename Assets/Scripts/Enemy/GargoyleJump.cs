d82b5550536ba8e1ca2a732c394025255194924b
exeynod <exeynod@gmail.com>
2019-12-06 14:01:15 +0300

    New boss and its room

---
+++ Assets/Scripts/Enemy/GargoyleJump.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GargoyleJump : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerManager player = collision.gameObject.GetComponent<PlayerManager>();
+            player.Knock(0f, 100f);
+        }
+    }
+}
---
+++ Assets/Scripts/WorldManager/LoadBossRoom.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadBossRoom : LoadNewLevel
+{
+    [SerializeField] List<string> bossesScenes;
+    [SerializeField] List<int> recommendedLevel;
+
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (Time.timeScale == 0)
+        {
+            return;
+        }
+        if (bossesScenes.Count != recommendedLevel.Count)
+        {
+            Debug.LogError("Wrong number of bosses rooms or necessary levels. Check it!");
+            return;
+        }
+        if (collision.tag == "Player")
+        {
+            PlayerManager player = collision.gameObject.GetComponent<PlayerManager>();
+            if (player.bossesProgres < bossesScenes.Count)
+            {
+                if (player.Level >= recommendedLevel[player.bossesProgres])
+                {
+                    LoadLevel(bossesScenes[player.bossesProgres]);
+                }
+            }
+        }
+    }
+}
