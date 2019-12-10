18c416e05baa260efbc24e78a5ba3f8033210f5c
exeynod <exeynod@gmail.com>
2019-12-10 17:19:40 +0300

    travis init

---
+++ ci/install-unity.sh
@@ -0,0 +1,40 @@
+#! /bin/sh
+
+UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
+UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/e20f6c7e5017/MacEditorInstaller/Unity.pkg"
+UNITY_WINDOWS_TARGET_PACKAGE_URL="https://download.unity3d.com/download_unity/e20f6c7e5017/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2019.2.13f1.pkg"
+
+
+# Downloads a file if it does not exist
+download() {
+
+	URL=$1
+	FILE=`basename "$URL"`
+	
+	# Downloads a package if it does not already exist in cache
+	if [ ! -e $UNITY_DOWNLOAD_CACHE/`basename "$URL"` ] ; then
+		echo "$FILE does not exist. Downloading from $URL: "
+		mkdir -p "$UNITY_DOWNLOAD_CACHE"
+		curl -o $UNITY_DOWNLOAD_CACHE/`basename "$URL"` "$URL"
+	else
+		echo "$FILE Exists. Skipping download."
+	fi
+}
+
+# Downloads and installs a package from an internet URL
+install() {
+	PACKAGE_URL=$1
+	download $1
+
+	echo "Installing `basename "$PACKAGE_URL"`"
+	sudo installer -dumplog -package $UNITY_DOWNLOAD_CACHE/`basename "$PACKAGE_URL"` -target /
+}
+
+
+
+echo "Contents of Unity Download Cache:"
+ls $UNITY_DOWNLOAD_CACHE
+
+echo "Installing Unity..."
+install $UNITY_OSX_PACKAGE_URL
+install $UNITY_WINDOWS_TARGET_PACKAGE_URL
